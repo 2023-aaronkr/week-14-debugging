84b7508878f206bfae9eb1db2ce727cf818ab3ef
Aaron Snowberger <jekkilekki@gmail.com>
2023-12-01 14:58:46 +0900

    Create practice3.c

---
+++ practice3.c
@@ -0,0 +1,28 @@
+// practice3.c
+// 예측 출력: This is a "buggy" program
+
+#include <stdio.h>
+
+int main(void){
+    int first;
+    int second;
+  
+    printf("Please enter a number: ");
+    scanf("%d",&first);
+  
+    printf("Please enter another number: ");
+    scanf("%d",&Second);
+  
+    printf("%d + %d = %d \n",first,second, frist+second);
+}
+
+/*
+질문에 답해 주세요.
+
+1. 컴파일할 때 어떤 오류 메시지가 나타났나요?
+
+2. 프로그램에 어떤 문제가 있었나요?
+
+3. 프로그램은 어떻게 수정하셨나요?
+
+*/
