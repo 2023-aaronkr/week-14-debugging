719dcd43d2dd2fde7cb13c86df9310bd394b980c
Aaron Snowberger <jekkilekki@gmail.com>
2023-12-01 14:58:02 +0900

    Create practice2.c

---
+++ practice2.c
@@ -0,0 +1,20 @@
+// practice2.c
+// 예측 출력: This is a "buggy" program
+
+#include <stdio.h>
+
+int main(void){
+    printf("This is a \"buggy" program\n");
+    return 0;
+}
+
+/*
+질문에 답해 주세요.
+
+1. 컴파일할 때 어떤 오류 메시지가 나타났나요?
+
+2. 프로그램에 어떤 문제가 있었나요?
+
+3. 프로그램은 어떻게 수정하셨나요?
+
+*/
