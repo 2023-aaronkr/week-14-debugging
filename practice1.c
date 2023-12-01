59cb1bce256b9c484406979f5ad1c165deb8c2ab
Aaron Snowberger <jekkilekki@gmail.com>
2023-12-01 14:57:09 +0900

    Create practice1.c

---
+++ practice1.c
@@ -0,0 +1,20 @@
+// practice1.c
+// 예측 출력: oh bug where are you?
+
+#include <stdio.h>
+
+Int main(void){
+    printf("oh bug where are you?\n");
+    return 0;
+}
+
+/*
+질문에 답해 주세요.
+
+1. 컴파일할 때 어떤 오류 메시지가 나타났나요?
+
+2. 프로그램에 어떤 문제가 있었나요?
+
+3. 프로그램은 어떻게 수정하셨나요?
+
+*/
